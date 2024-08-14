753766ab60452e17b6f17ddf808cb031f20994c2
Efren Lim <elim@linuxfoundation.org>
2024-08-14 09:44:28 +0800

    added assets in ng package
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- projects/lfx-component-lib/src/public-api.ts
+++ projects/lfx-component-lib/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of lfx-component-lib
  */
 
-export { default as colorPalette } from './lib/configs/color-palette.json';
 export * from './lib/lfx-component-lib.service';
 export * from './lib/lfx-component-lib.component';
