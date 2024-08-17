ab3a19abba18abe4bbedd315e97c57a134bb033b
AlexDanilov <adanilov@contractor.linuxfoundation.org>
2024-08-17 17:49:47 +0200

    Toast story refactoring

--- projects/lfx-component-lib/src/lib/containers/toast/__docs__/toast.stories.ts
+++ projects/lfx-component-lib/src/lib/containers/toast/__docs__/toast.stories.ts
@@ -1,29 +1,54 @@
-import type { Meta, StoryObj } from '@storybook/angular';
-import { ToastComponent } from '../toast.component';
+import { Meta, StoryObj } from '@storybook/angular';
+import { IType, ToastComponent } from '../toast.component';
+
+type Story = StoryObj<ToastComponent>;
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta: Meta<ToastComponent> = {
   title: 'LFX Components/Containers/Toast',
   component: ToastComponent,
   tags: ['autodocs'],
+  render: (args) => ({
+    template: `
+      <lfx-toast type="${args.type}">
+        <h1>Title</h1>
+        <p>Body paragraph</p>
+      </lfx-toast>
+    `,
+  }),
   argTypes: {
     type: {
       control: 'select',
-      options: ['notice', 'warning', 'success', 'error'],
-      defaultValue: 'notice',
+      options: ['notice', 'warning', 'success', 'error'] as IType[],
     },
+  },
+  args: {
+    type: 'notice' as IType,
   },
 };
 
 export default meta;
-type Story = StoryObj<ToastComponent>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Info: Story = {
-  render: (args: any) => ({
-    template: `<lfx-toast type="${args.type}">
-      <h1>Title</h1>
-      <p>Body paragraph</p>
-    </lfx-toast>`,
-  }),
+export const Notice: Story = {
+  args: {
+    type: 'notice' as IType,
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    type: 'warning' as IType,
+  },
+};
+
+export const Success: Story = {
+  args: {
+    type: 'success' as IType,
+  },
+};
+
+export const Error: Story = {
+  args: {
+    type: 'error' as IType,
+  },
 };
--- projects/lfx-component-lib/src/lib/containers/toast/toast.component.ts
+++ projects/lfx-component-lib/src/lib/containers/toast/toast.component.ts
@@ -1,6 +1,7 @@
 import { Component, input } from '@angular/core';
 
-type IType = 'notice' | 'warning' | 'success' | 'error';
+export type IType = 'notice' | 'warning' | 'success' | 'error';
+
 @Component({
   selector: 'lfx-toast',
   standalone: true,
